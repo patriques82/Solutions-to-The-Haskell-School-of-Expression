1f979a3da4e8d0d1c7a9a420ede20d120009937d
Patrik Nygren <patriknygren82@gmail.com>
2014-07-08 23:13:30 +0200

    Finished exercise 1, started on 2

---
+++ ch1.hs
@@ -0,0 +1,49 @@
+module Ch1 where
+
+simple a b c = a * (b + c)
+
+{- 1.1
+Write out all the steps in the calculation of the value of
+simple (simple 2 3 4) 5 6
+
+simple (simple 2 3 4) 5 6
+    => (simple 2 3 4) * (5 + 6)
+    => (2 * (3 + 4)) * (5 + 6)
+    => (2 * 7) * (5 + 6)
+    => 14 * (5 + 6)
+    => 14 * 11
+    => 154
+-}
+
+{- 1.2
+Prove by calculation that simple (a - b) a b => a^2 - b^2
+
+simple (a - b) a b
+    => (a - b) * (a + b)
+    => (a + (-b)) * (a + b)
+    => (a^2 + ab) + (-ba + (-b^2))
+    => a^2 - b^2
+-}
+
+{- 1.3
+Identify the well-typed expressions in the following and,
+for each, give its proper type:
+
+[(2,3), (4,5)]
+[(2,3), (4,5)] :: Num t => [(t, t)]
+
+['z', 42]
+['z', 42] :: error "No instance for (Num Char) arising from the literal `42'"
+
+('z', -42)
+('z', -42) :: Num t => (Char, t)
+
+simple 'a' 'b' 'c'
+simple 'a' 'b' 'c' :: error
+
+(simple 1 2 3, simple)
+(simple 1 2 3, simple) :: Num t => (t, (t -> t -> t -> t))
+-}
+
+
+
---
+++ ch2.hs
@@ -0,0 +1,48 @@
+module Shape where
+
+data Shape = Rectangle Side Side
+           | Ellipse Radius Radius
+           | RtTriangle Side Side
+           | Polygon [Vertex]
+    deriving Show
+
+type Radius = Float
+type Side   = Float
+type Vertex = (Float, Float)
+
+square s = Rectangle s s
+
+circle r = Ellipse r r
+
+{- 2.1
+Define functions "rectangle" and "rtTriangle" in terns of
+Polygon.
+-}
+
+rectangle :: Side -> Side -> Shape
+rectangle s1 s2 = Polygon [(0, 0), (s1, 0), (s1, s2), (0, s2)]
+
+rtTriangle :: Side -> Side -> Shape
+rtTriangle s1 s2 = Polygon [(0, 0), (s1, s2), (0, s2)]
+
+{- 2.2
+Define a function "regularPolygon :: Int -> Side -> Shape"
+such that "regularPolygon n s" is a regular polygon with n sides, each of
+length s. (Hint: Consider using some of Haskell´s trigonometric functions,
+such as "sin :: Float -> Float", "cos :: Float -> Float", and "tan :: Float -> Float".)
+-}
+
+-- This function considers every polygon defined within the borders of a circle
+-- where each vertex touches the circles circumference, the center of it is the origin
+regularPolygon :: Int -> Side -> Shape
+regularPolygon n s = Polygon $ vertices n
+    where
+        angle  = (pi * 2) / fromIntegral n
+        radius = sin angle * (s / 2)
+        vertices nr | nr > 0    = (x, y) : vertices (nr - 1) -- recursive definition
+                    | otherwise = []
+            where
+                -- increment angle with nr for each new vertex
+                x = cos (angle * fromIntegral nr) * radius
+                y = sin (angle * fromIntegral nr) * radius
+
