28424a9a8a98d60b3017d9788e7c4ed097a96445
Patrik Nygren <patriknygren82@gmail.com>
2014-07-22 10:01:10 +0200

    started 13.2

--- School-of-Expression/src/ch13.hs
+++ School-of-Expression/src/ch13.hs
@@ -1,6 +1,7 @@
 module Ch13 where
 
 import Region
+import Picture
 import Animation
 
 {- 13.1
@@ -13,5 +14,42 @@ instance Combine Region where
 	over  = Union
 
 instance Combine Shape where
-	empty = Polygon []
-	over  = 
+	empty = undefined -- Can´t come up with nice way of doing this. ??
+	over  = undefined
+
+-- Since Combine [a] is already defined in Animation there is no need
+-- to redo it. a is a polymorphic type variable and could be a Char
+-- String = [Char] => Combine String is not necessary
+
+instance Combine Float where
+	empty = 0
+	over  = (+)
+
+{- 13.2
+Make the planets more realistic:
+-}
+
+-- Result of implementing spec 1,2:
+
+-- earths perimeter around sun
+perimeter :: (Behavior Time, Behavior Time)
+perimeter = (2.5 * sin time, 2.5 * cos time)
+
+moon :: Behavior Picture
+moon = let (x, y) = perimeter
+			 in reg (lift0 White) $
+						translate (x + 0.35 * sin (5 * time),
+												y - 0.35 * cos (5 * time)) $
+							shape $ ell 0.1 0.1
+
+earth :: Behavior Picture
+earth = reg (lift0 Blue) $
+					translate perimeter $
+						shape $ ell 0.25 0.25
+
+sun :: Behavior Picture
+sun = reg yellow $ shape $ ell 0.5 0.5
+
+orbits :: IO ()
+orbits = animateB "Orbits" $ overMany [moon, earth, sun]
+
