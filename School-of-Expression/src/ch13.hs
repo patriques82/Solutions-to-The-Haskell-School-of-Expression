7bb0d580219d5a6484a5c6861a12880400ac2af8
Patrik Nygren <patriknygren82@gmail.com>
2014-07-22 13:50:17 +0200

    Finished planets: 13.2

--- School-of-Expression/src/ch13.hs
+++ School-of-Expression/src/ch13.hs
@@ -29,27 +29,61 @@ instance Combine Float where
 Make the planets more realistic:
 -}
 
--- Result of implementing spec 1,2:
+-- Result of implementing spec 1,2,3 (not 4):
 
--- earths perimeter around sun
-perimeter :: (Behavior Time, Behavior Time)
-perimeter = (2.5 * sin time, 2.5 * cos time)
+earthsOrbit :: (Behavior Time, Behavior Time)
+earthsOrbit = ((14*pi/16) * sin time,
+							 (3*pi/16) * cos time)
+
+moonsOrbit :: (Behavior Time, Behavior Time)
+moonsOrbit
+	= let (x, y) = earthsOrbit
+		in (x + (4*pi/16) * cos (5 * time),
+				y - (pi/16) * sin (5 * time))
 
 moon :: Behavior Picture
-moon = let (x, y) = perimeter
-			 in reg (lift0 White) $
-						translate (x + 0.35 * sin (5 * time),
-												y - 0.35 * cos (5 * time)) $
-							shape $ ell 0.1 0.1
+moon = reg (lift0 White) $ translate moonsOrbit $
+												 		shape $ ell 0.1 0.1
 
 earth :: Behavior Picture
-earth = reg (lift0 Blue) $
-					translate perimeter $
-						shape $ ell 0.25 0.25
+earth = reg (lift0 Blue) $ translate earthsOrbit $
+														shape $ ell 0.25 0.25
 
 sun :: Behavior Picture
-sun = reg yellow $ shape $ ell 0.5 0.5
+sun = reg yellow $ shape $ ell 0.7 0.7
 
-orbits :: IO ()
-orbits = animateB "Orbits" $ overMany [moon, earth, sun]
+(<*) :: Ord a => Behavior a -> Behavior a -> Behavior Bool
+(<*) = lift2 (<)
+
+planetOrder :: Behavior Picture
+planetOrder = cond (cos time <* 0)       -- if earths y-pos is under suns
+								(lift2 over xs sun)      -- then sun behind earth
+								(lift2 over sun xs)      -- else earth behind sun
+	where xs = cond (cos time <* (cos time - (sin (5 * time))))
+								(lift2 over earth moon)  -- moon behind earth
+								(lift2 over moon earth)  -- earth behind moon
+
+planets' :: IO ()
+planets' = animateB "Orbits" planetOrder
+
+
+{- 13.3
+Build a clock
+-}
+
+clock :: IO ()
+clock = animateB "Clock" undefined
+
+
+
+
+
+
+
+
+
+
+
+
+
 
