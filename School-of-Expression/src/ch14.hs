b6a70a2068d8b02b6577911f3f18d8205ecd4b67
Patrik Nygren <patriknygren82@gmail.com>
2014-07-23 23:44:07 +0200

    finished some exercises: 14.2,3,4

--- School-of-Expression/src/ch14.hs
+++ School-of-Expression/src/ch14.hs
@@ -49,6 +49,115 @@ stream = 1 : numerators stream
 -- set amount of primes to use
 ps = take 5 primes
 
+{- 14.2
+Prove as many of the properties as you can of the following proporties, but
+for infinite lists (streams).
+
+map properties:
+1. map (\x -> x) = \x -> x
+
+Base case:
+map (\x -> x) bottom
+=> bottom
+=> (\x -> x) bottom
+
+Inductive case:
+map (\x -> x) (x:xs)
+=> (\x -> x) $ x : map (\x -> x) xs
+=> x : map (\x -> x) xs
+=> inductive step
+=> x : (\x -> x) xs
+=> x : xs
+=> (\x -> x) (x:xs)
+
+
+2. map (f . g) = map f . map g
+
+Base case:
+map (f . g) bottom
+=> bottom
+=> map f . map g $ bottom
+
+Inductive case:
+map (f . g) (x:xs)
+=> f . g $ x : map (f . g) xs
+=> f (g x) : map (f . g) xs
+=> inductive step
+=> f (g x) : map f . map g $ xs
+=> map f . map g $ (x:xs)
+
+-- skipped these
+map f . tail = tail . map f
+map f . reverse = concat . map (map f)
+map f (xs ++ ys) = map f xs ++ map f ys
+f . head = head . map f
+(++) properties:
+(xs ++ ys) ++ zs = xs ++ (ys ++ zs)
+xs ++ [] = [] ++ xs = xs
+
+-}
+
+{- 14.3
+Prove that fib n = fibs !! n for all n >= 0
+
+fib :: Integer -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+fibs :: [Integer]
+fibs = 1 : 1 : zipWith (+) fibs (tail fibs)
+
+Base case:
+fib 0
+=> 1
+=> fibs !! 0
+
+fib 1
+=> 1
+=> fibs !! 1
+
+Inductive case:
+fib (n+1)
+=> fib n + fib (n-1)
+=> inductive step
+=> fibs !! n + fibs !! (n-1)
+=> fibs !! (n+1)
+
+-}
+
+{- 14.4
+Describe each of the following infinite lists.
+
+xs = 1 : map (*2) xs
+
+takes the output of itself (xs) and maps (*2) to each of its elements:
+=> 1 : map (*2) (1:xs)
+=> 1 : 2 : map (*2) (2:xs)
+=> 1 : 2 : 4 : map (*2) (4:xs)
+=> 1 : 2 : 4 : 8 : map (*2) (8:xs)
+...
+this leads to a list of the powers of 2
+
+
+ys = [1] : [zipWith (+) (0:q) (q++[0]) | q <- ys]
+
+takes the output if itself (ys) and zips together the output with itself
+where the first list is appended with a zero at the beginning and the latter
+with a zero at the end.
+=> [1] : zipWith (+) [0,1] [1,0] : [zipWith (+) (0:q) (q++[0]) | q <- ys]
+=> [1] : [1,1] : [zipWith (+) (0:q) (q++[0]) | q <- ys]
+=> [1] : [1,1] : zipWith (+) [0,1,1] [1,1,0] : [zipWith (+) (0:q) (q++[0]) | q <- ys]
+=> [1] : [1,1] : [1,2,1] : [zipWith (+) (0:q) (q++[0]) | q <- ys]
+=> [1] : [1,1] : [1,2,1] : zipWith (+) [0,1,2,1] [1,2,1,0] : [zipWith (+) (0:q) (q++[0]) | q <- ys]
+=> [1] : [1,1] : [1,2,1] : [1,3,3,1] : [zipWith (+) (0:q) (q++[0]) | q <- ys]
+...
+this leads to pascals triangle!
+
+-}
+
+
+
 
 
 
